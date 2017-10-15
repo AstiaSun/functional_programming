c4bbe9a3ce887f292a423382d2aac046ab326e79
AstiaSun <nastiasunrise@gmail.com>
2017-10-15 20:39:51 +0300

    add author db methods

--- lab1/src/Author.hs
+++ lab1/src/Author.hs
@@ -1,2 +1,35 @@
 module Author where
 
+import Prelude hiding (read)
+import Database.HDBC
+import Database.HDBC.PostgreSQL
+import qualified Data.ByteString.Char8 as BS
+
+type Id = Integer
+type Name = String
+type Surname = String
+
+getAllAuthors :: IConnection a => a -> IO[(Id, Name, Surname)]
+getAllAuthors connection = do
+    result <- quickQuery' connection query [SQLInteger id]
+    return $ map unpack result
+    where
+        query = "select * from authors order by id"
+        unpack [SqlInteger aid, SqlByteString name, SqlByteString surname] =
+               (aid, BS.unpack name, BS.unpack surname)
+            {-unpack x = error $ "Unexpected result: " ++ show x-}
+
+addAuthor :: IConnection a => Name -> Surname -> Id -> IO Bool
+addAuthor name surname connection =
+    withTransaction connection (createAuthor' name surname connection)
+
+createAuthor' name surname connection = do
+    isChanged <- run connection query [SqlString name, SqlString surname]
+    return $ isChanged == 1
+    where query = "insert into authors(name, surname) values (?, ?)"
+
+deleteAuthor :: IConnection a => Id -> IO Bool
+deleteAuthor id connection = do
+    isChanged <- run connection query [SqlInteger id]
+    return $ isChanged == 1
+    where query = "delete from authors where id=?"
--- lab1/src/Main.hs
+++ lab1/src/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
-import Database.HDCB
-import Database.HDBC.PostgreSQL
+import Database.HDBC
+import Database.HDBC.PostgreSQL (connectPostgreSQL)
+import Author
+
+main = do
+    connection <- connectPostgreSQL("host=localhost dbname=faculty_newspaper user=redactor password=qwerty")
+    putStrLn "-------------Authors--------------"
+    allAuthorsResult = getAllAuthors connection
+    putStrLn show allAuthorsResult
+    commit connection
+    disconnect connection
+    return ()
